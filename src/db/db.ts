f4e3cfc9cca3e71e1f673d48621c3fb1188d6ac1
tinnyterr <l.mcdonald@dmnet.uk>
2025-12-16 23:18:55 +0000

    more db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,12 @@
 import { Database } from "bun:sqlite";
 import { schema } from "./schema";
 import path from "node:path"
+import fs from "node:fs"
 
 const DB_PATH = process.env.IMAGER_DB
-  ? path.resolve(process.env.IMAGER_DB)
+  ? fs.statSync(process.env.IMAGER_DB).isFile()
+    ? process.env.IMAGER_DB
+    : path.resolve(process.env.IMAGER_DB, "../index.db")
   : path.resolve(import.meta.dir, "../index.db");
 
 export const db = new Database(DB_PATH, {
